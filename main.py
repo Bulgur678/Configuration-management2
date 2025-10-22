4ce603a199f1da6fa6e516cf6050f4ed24595cef
Bulgur678 <abcd505@yandex.ru>
2025-10-22 13:30:35 +0300

    Stage 1: №2

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import argparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def command_line():
+    args = {}
+    parser = argparse.ArgumentParser (
+        description='CLI'
+    )
+
+    #Имя пакета
+    parser.add_argument( "-p", '--package-name',
+                        type=str,
+                        help="Введите ваше имя",
+                        required=True)
+    # URL репозитория
+    parser.add_argument('-r', '--repo-url',
+                        type=str,
+                        default='',
+                        help='URL-адрес репозитория или путь к файлу тестового репозитория')
+
+    # Режим работы с репозиторием
+    parser.add_argument( '-m', '--repo-mode',
+                        type=str,
+                        choices=['local', 'remote', 'test'],
+                        default='local',
+                        help='Режим работы с тестовым репозиторием (local, remote, test)')
+
+    # Имя файла для изображения
+    parser.add_argument('-o', '--output-image',
+                        type=str,
+                        help='Имя сгенерированного файла с изображением графа')
+
+    # Режим ASCII-дерева
+    parser.add_argument('-a', '--ascii-tree',
+                        action='store_true',
+                        help='Режим вывода зависимостей в формате ASCII-дерева')
+
+    args = parser.parse_args()  #Парсинг аргументов
+
+    if not args.package_name:
+        raise  ValueError("package_name cannot be empty")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+command_line()
